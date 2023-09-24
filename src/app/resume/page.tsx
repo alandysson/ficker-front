af0d2efce811f72f5c2fd2228fcdadecf5c51d58
Iris Mayara <mayaraim.114@gmail.com>
2023-09-23 21:20:24 -0300

    feat: oculta saldo e edita gasto planejado

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,27 +6,125 @@ import { Button, Col, Row, Typography } from "antd";
 import styles from "./resume.module.scss";
 import MyCategoriesList from "@/components/MyCategoriesList";
 import LastTransactionsList from "@/components/LastTransactionsList";
+import { useState } from "react";
 
 const Resume = () => {
+  const today = new Date();
+  const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+  const monthNames = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  const monthName = monthNames[today.getMonth()];
+
+  const dateRange = `${firstDayOfMonth.getDate()} de ${monthName} - ${lastDayOfMonth.getDate()} de ${monthName}`;
+
+  const [showSaldo, setShowSaldo] = useState(true);
+  const [iconShowSaldo, setIconShowSaldo] = useState(
+    "/icons/icon-hide-saldo.svg"
+  );
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [gastoPlanejado, setGastoPlanejado] = useState("0");
+
+  const handleClickShowSaldo = () => {
+    setShowSaldo(!showSaldo);
+    if (showSaldo) {
+      setIconShowSaldo("/icons/icon-show-saldo.svg");
+    } else {
+      setIconShowSaldo("/icons/icon-hide-saldo.svg");
+    }
+  };
+
+  const handleClickEditGastoPlanejado = () => {
+    setIsEditMode(true);
+  }
+
+  const handleBlur = () => {
+    setIsEditMode(false);
+    // Faça a requisição de atualização aqui com o novo valor (gastoPlanejado)
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      setIsEditMode(false);
+      // Faça a requisição de atualização aqui com o novo valor (gastoPlanejado)
+    }
+  };
+
+  const formatCurrency = (value: any) => {
+    const formattedValue = parseFloat(value).toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+    return formattedValue;
+  };
+
   return (
     <div>
       <div style={{ background: "#fff", padding: 10, alignItems: "center" }}>
-        <Link href={"/"} style={{ background: "#fff", padding: 10, alignItems: "center" }}>
+        <Link
+          href={"/"}
+          style={{ background: "#fff", padding: 10, alignItems: "center" }}
+        >
           <Image src="/logo.png" alt="Logo" width={130} height={27} />
         </Link>
       </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <CustomMenu />
         <Col style={{ padding: "10px 30px" }} xl={20} lg={19} md={22} xs={24}>
-          <div>
+          <Row align={"middle"} justify={"space-between"}>
             <div>
-              <h3>Olá</h3>
+              <div>
+                <h3>Olá</h3>
+              </div>
             </div>
-          </div>
+            <div>
+              <span style={{ color: "#808191", fontSize: "small" }}>
+                {dateRange}
+              </span>
+            </div>
+          </Row>
           <Row justify={"space-between"}>
-            <Col className={styles.balance} xl={8} lg={9} md={9} xs={20} style={{ marginBottom: 10 }}>
-              <p className={styles.balance_description}>Saldo</p>
-              <p className={styles.balance_title}>R$ 0</p>
+            <Col
+              className={styles.balance}
+              style={{ marginRight: 20, marginBottom: 10 }}
+              xl={11}
+              lg={11}
+              md={12}
+              xs={15}
+            >
+              <Row align={"middle"} justify={"space-between"}>
+                <Col style={{ marginRight: 10 }}>
+                  <p className={styles.balance_description}>Saldo</p>
+                  <p className={styles.balance_title}>
+                    {showSaldo ? "R$ 0" : "****"}
+                  </p>
+                </Col>
+                <Col>
+                  <Button type="text">
+                    <Image
+                      src={iconShowSaldo}
+                      alt="Hide"
+                      width={20}
+                      height={20}
+                      onClick={handleClickShowSaldo}
+                    />
+                  </Button>
+                </Col>
+              </Row>
             </Col>
             <Col xl={9} lg={14} md={15} xs={24}>
               <Row>
@@ -38,16 +136,47 @@ const Resume = () => {
                   md={12}
                   xs={15}
                 >
-                  <Row align={"middle"}>
+                  <Row align={"middle"} justify={"space-between"}>
                     <Col style={{ marginRight: 10 }}>
-                      <p className={styles.balance_description}>Gasto Planejado</p>
-                      <p className={styles.balance_title}>R$ 0</p>
+                      <p className={styles.balance_description}>
+                        Gasto Planejado
+                      </p>
+                      {isEditMode ? (
+                          <input
+                            type="number"
+                            step="0.01"
+                            min="0"
+                            value={gastoPlanejado}
+                            onChange={(e) => setGastoPlanejado(e.target.value)}
+                            onBlur={handleBlur}
+                            onKeyDown={handleKeyDown}
+                            autoFocus
+                            style={{
+                              border: 'none', 
+                              outline: 'none', 
+                              background: 'none', 
+                              boxShadow: 'none' ,
+                              width: "90%",
+                              fontSize: "large",
+                              padding: "15px"
+                            }}
+                          />
+                      ) : (
+                        <p className={styles.balance_title}>{formatCurrency(gastoPlanejado)}</p>
+                      )}
                     </Col>
+                    {!isEditMode ? (
                     <Col>
                       <Button type="text">
-                        <Image src="/edit.png" alt="Editar" width={20} height={20} />
+                        <Image
+                          src="/edit.png"
+                          alt="Editar"
+                          width={20}
+                          height={20}
+                          onClick={handleClickEditGastoPlanejado}
+                        />
                       </Button>
-                    </Col>
+                    </Col>) : ''}
                   </Row>
                 </Col>
                 <Col className={styles.balance} xl={11} lg={11} md={10} xs={15}>
