2291214fe879b686836e35cf66adcf821c1dda8c
alandysson <alandyssom@gmail.com>
2024-03-27 16:31:49 -0300

    chore: add jest-junit this lib get the results of tests

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  reporters: ["default", ["jest-junit", {outputDirectory: "reports", outputName: "jest-junit.xml"}]],
 };
 module.exports = createJestConfig(customJestConfig);
