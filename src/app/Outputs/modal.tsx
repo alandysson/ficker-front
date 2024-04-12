655cc50cd2761c408c4833f021afdb7aa155d23e
alandysson <alandyssom@gmail.com>
2024-04-12 10:05:49 -0300

    refactor(outputs/Modal): remove unused packages

--- src/app/Outputs/modal.tsx
+++ src/app/Outputs/modal.tsx
@@ -2,7 +2,6 @@
 import { request } from "@/service/api";
 import styles from "../EnterTransaction/entertransaction.module.scss";
 import { Modal, Col, DatePicker, Row, Select, Form, Button, Input, message } from "antd";
-import type { DatePickerProps } from "antd";
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import { Card } from "@/interfaces";
