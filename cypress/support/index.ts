b2209aa3917d39291b686130dba2b48864bf0564
alandysson <alandyssom@gmail.com>
2024-05-15 16:01:38 -0300

    chore(app): change the cypressconfig

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -2,7 +2,7 @@
 
 declare namespace Cypress {
   interface Chainable<Subject = any> {
-    login(email: string, password: string): Chainable<any>;
+    login(email: string, password: string): Chainable<Subject>;
     dataTest(value: string): Chainable<JQuery<HTMLElement>>;
   }
 }
